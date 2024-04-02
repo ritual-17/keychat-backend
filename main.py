439a8f3014ef27942949c9d65009c1dcc53e073a
Ibrahim Quraishi <58309356+Ibbs620@users.noreply.github.com>
2024-04-02 20:08:27 +0000

    Added basic socketio server

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
+import eventlet
+import socketio
 
 app = FastAPI()
 
@@ -11,3 +12,24 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+clients = []
+
+@sio.event
+def connect(sid, env):
+  clients.append(sid)
+  print('connect ', sid)
+
+@sio.event
+def disconnect(sid):
+  clients.remove(sid)
+  print('disconnect ', sid)
+
+@sio.event
+def connected(sid, data):
+  print(data)
+
+if __name__ == '__main__':
+  eventlet.wsgi.server(eventlet.listen(('',8080)), app)
