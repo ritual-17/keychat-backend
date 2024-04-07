eb24e706edc68984234f1b0f44a789f72f89e44f
Ibrahim Quraishi <58309356+Ibbs620@users.noreply.github.com>
2024-04-07 15:53:12 +0000

    Added some events

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import eventlet
 import socketio
 import threading
-
+import base64
 import KDC
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
-user_secrets = {}
-service_secrets = {}
-kdc = KDC.KDC(user_secrets, service_secrets)
 
 usernames = {
-  "alice": "password123",
-  "bob" : "password456",
-  "cody": "password789"
+  "alice": "password12345678",
+  "bob" : "password87654321",
+  "cody": "12345678password"
 }
+
+user_secrets = {
+  "alice": usernames['alice'].encode(),
+  "bob": usernames['bob'].encode(),
+  "cody": usernames['cody'].encode()
+}
+
+contacts = {
+  "alice" : ["bob"],
+  "bob" : ["alice"],
+  "cody": []
+}
+online_users = []
+
+kdc = KDC.KDC(user_secrets, user_secrets)
 
 @sio.event
 def register(sid, username):
   kdc.add_user(username, sid)
-  return kdc.register(username)
+  return base64.b64encode(kdc.register(username)).decode()
 
 @sio.event
 def disconnect(sid):
@@ -35,7 +47,19 @@ def refresh_keys():
     threading.Event().wait(300) # refresh every 5 minutes
     for user in kdc.active_users:
       sio.emit('start_key_update', to=user)
-      
+
+@sio.event
+def get_contacts(sid, data):
+  return ",".join(contacts[data])
+
+@sio.event
+def add_contact(sid, data):
+  contact, user = data.split(',')
+  if contact in usernames.keys() and contact not in contacts[user]:
+    contacts[user].append(contact)
+    return 100
+  else:
+    return 200
 
 @sio.event
 def update_key(sid, tgt):
@@ -46,11 +70,23 @@ def get_login(sid, data):
   username, password = data.split(",")
   if username in usernames.keys():
     if usernames[username] == password:
-      sio.emit("login_success")
+      online_users.append(username)
+      return 100
     else:
-      sio.emit("login_failure")
+      return 200
   else:
-    sio.emit("login_failure")
+    return 200
+
+@sio.event
+def request_logs(sid, username, recipient, ticket):
+  pass
+
+def get_user_from_sid(sid):
+  for username in online_users.keys():
+    if online_users[username] == sid:
+      online_users.pop(username)
+      return username
+  return "unknown"
 
 if __name__ == '__main__':
   threading.Thread(target=refresh_keys, daemon=True).start()
